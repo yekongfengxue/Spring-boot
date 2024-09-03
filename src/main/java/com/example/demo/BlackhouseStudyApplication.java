8fedfee6e2f30b656a3075bd0f9249eb80505546
yekong xuefeng <1209908879@qq.com>
2024-09-04 00:33:19 +0800

    Default Changelist

---
+++ src/main/java/com/example/demo/BlackhouseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//启动类
+@SpringBootApplication
+public class BlackhouseStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackhouseStudyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "孙皓天与7月28日学习spring boot" ;
+
+    }
+
+}
